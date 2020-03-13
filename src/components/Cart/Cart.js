455040cd38fb453eb89443697290230455fd2395
akibp1 <mda588825@gmail.com>
2020-03-14 02:16:11 +0600

    hoiche motamoti

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+
+    let total = 0;
+    for (let i = 0; i< cart.length; i++){
+        const product = cart[i];
+        total =(total + product.price)
+    }
+    let shipping = 0;
+    if (total > 15){
+        shipping = 4.99;
+    }
+    else if (total > 35){
+        shipping = 0;
+    }
+    else if(total > 0){
+        shipping = 12.99
+    }
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number (tax)).toFixed(2);
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number (precision);
+    }
+    return (
+        <div>
+            <h4>Order Summary</h4>
+    <h5>Items ordered: {cart.length}</h5>
+    <p>Product Price: {formatNumber(total)}</p>
+    <p>Shipping Cost:{shipping}</p>
+    <p><small>Tax + Vat:{tax}</small></p>
+    <p>Total Price:{grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
